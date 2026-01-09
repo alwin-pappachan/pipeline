c6af313b7f19bdcffbd49199aba3307f9efee414
alwin-pappachan <alwinpappachan56@gmail.com>
2026-01-09 11:09:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>alwin pappachan</h1>
+      <h1>alwin pappachan SCENE aneee</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
